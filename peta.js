a95b96ba9dbad12a9bd463f752c08edffa290590
Vitradisa Pratama <vitadisa@pptik.itb.ac.id>
2019-04-30 11:27:40 +0700

    rapi rapi source code

--- peta.js
+++ peta.js
@@ -11,27 +11,6 @@ var gambarPetaDasar = L.tileLayer('http://vectormap.pptik.id/styles/klokantech-b
     attribution: '&copy; <a href="https://vectormap.pptik.id">PPTIK</a>'
 }).addTo(mymap);
 
-var jsonLayer = L.geoJson().addTo(mymap);
-
-//Load Json Data
-gambarPetaDasar.on("tileload", function( event ){
-	var z = event.coords.z;
-	var x = event.coords.x;
-	var y = event.coords.y;
-    
-	var jsonString = "http://localhost/" + z + "-" + x + "-" +  y + ".geojson";
-    
-	$.getJSON( jsonString, function(json){
-		jsonLayer.addData(json);
-	} );
-    
-});
-
-//clear data ketika zoomend
-mymap.on("zoomend", function(){
-    jsonLayer.clearLayers();
-});
-
 //load layer objek spesifik di mapi
 $.ajax({
     type: "GET",
